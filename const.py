11f4e4da9eb063ef7008e13059eb37ae7d3d4894
Adrian L. <adrian@luparu.com>
2025-09-22 18:18:34 -0400

    Create const.py

---
+++ const.py
@@ -0,0 +1,14 @@
+DOMAIN = "zoneminder_db"
+
+CONF_DB_HOST = "db_host"
+CONF_DB_PORT = "db_port"
+CONF_DB_NAME = "db_name"
+CONF_DB_USER = "db_user"
+CONF_DB_PASSWORD = "db_password"
+CONF_POLL_INTERVAL = "poll_interval"
+CONF_LOOKBACK_WINDOW = "lookback_window"
+
+DEFAULT_POLL_INTERVAL = 60
+DEFAULT_LOOKBACK_WINDOW = 5
+
+
