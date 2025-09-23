3718c699912247bf56da9ae731bbd53b6625ca6f
Adrian L. <adrian@luparu.com>
2025-09-22 20:03:12 -0400

    Update const.py

--- const.py
+++ const.py
@@ -1,14 +1,19 @@
-DOMAIN = "zoneminder_db"
+DOMAIN                  = "zoneminder_db"
 
-CONF_DB_HOST = "db_host"
-CONF_DB_PORT = "db_port"
-CONF_DB_NAME = "db_name"
-CONF_DB_USER = "db_user"
-CONF_DB_PASSWORD = "db_password"
-CONF_POLL_INTERVAL = "poll_interval"
-CONF_LOOKBACK_WINDOW = "lookback_window"
+CONF_DB_HOST            = "host"
+CONF_DB_PORT            = "port"
+CONF_DB_NAME            = "database"
+CONF_DB_USER            = "username"
+CONF_DB_PASSWORD        = "password"
 
-DEFAULT_POLL_INTERVAL = 60
-DEFAULT_LOOKBACK_WINDOW = 5
+CONF_POLL_INTERVAL      = "poll_interval"
+CONF_LOOKBACK_WINDOW    = "lookback_window"
+CONF_BIN_INTERVAL       = "bin_interval"
+
+DEFAULT_POLL_INTERVAL   = 60    # seconds
+DEFAULT_LOOKBACK_WINDOW = 60    # minutes
+DEFAULT_BIN_INTERVAL    = 5     # minutes
+
+
 
 
