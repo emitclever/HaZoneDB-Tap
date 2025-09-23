eda728e0f552b22800f950fe6549a046be56f762
Adrian L. <adrian@luparu.com>
2025-09-22 20:02:44 -0400

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -1,4 +1,5 @@
 import voluptuous as vol
+
 from homeassistant import config_entries
 from homeassistant.core import callback
 
@@ -11,18 +12,19 @@ from .const import (
     CONF_DB_PASSWORD,
     CONF_POLL_INTERVAL,
     CONF_LOOKBACK_WINDOW,
+    CONF_BIN_INTERVAL,
     DEFAULT_POLL_INTERVAL,
     DEFAULT_LOOKBACK_WINDOW,
+    DEFAULT_BIN_INTERVAL,
 )
-
-from .options_flow import ZMOptionsFlowHandler   # ← ← ← ADD THIS LINE
+from .options_flow import ZMOptionsFlowHandler
 
 class ZMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         errors = {}
-        if user_input:
+        if user_input is not None:
             return self.async_create_entry(title=user_input[CONF_DB_HOST], data=user_input)
 
         schema = vol.Schema({
@@ -30,9 +32,10 @@ class ZMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             vol.Required(CONF_DB_PORT, default=3306): int,
             vol.Required(CONF_DB_NAME, default="zm"): str,
             vol.Required(CONF_DB_USER, default="root"): str,
-            vol.Required(CONF_DB_PASSWORD): str,
+            vol.Required(CONF_DB_PASSWORD, default=""): str,
             vol.Optional(CONF_POLL_INTERVAL, default=DEFAULT_POLL_INTERVAL): int,
             vol.Optional(CONF_LOOKBACK_WINDOW, default=DEFAULT_LOOKBACK_WINDOW): int,
+            vol.Optional(CONF_BIN_INTERVAL, default=DEFAULT_BIN_INTERVAL): int,
         })
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
@@ -41,3 +44,8 @@ class ZMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(config_entry):
         return ZMOptionsFlowHandler(config_entry)
+
+
+
+
+
