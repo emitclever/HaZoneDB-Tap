1de77aa33c06dea8b0456e7876cdc33c3811d84e
Adrian L. <adrian@luparu.com>
2025-09-22 20:05:09 -0400

    Update option_flow.py

--- option_flow.py
+++ option_flow.py
@@ -1,11 +1,14 @@
 import voluptuous as vol
+
 from homeassistant import config_entries
 
 from .const import (
     CONF_POLL_INTERVAL,
     CONF_LOOKBACK_WINDOW,
+    CONF_BIN_INTERVAL,
     DEFAULT_POLL_INTERVAL,
     DEFAULT_LOOKBACK_WINDOW,
+    DEFAULT_BIN_INTERVAL,
 )
 
 class ZMOptionsFlowHandler(config_entries.OptionsFlow):
@@ -13,20 +16,26 @@ class ZMOptionsFlowHandler(config_entries.OptionsFlow):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
-        if user_input:
-            return self.async_create_entry(data=user_input)
+        data = self.config_entry.options or {}
+        if user_input is not None:
+            return self.async_create_entry(title="", data={**data, **user_input})
 
-        current = self.config_entry.options
         schema = vol.Schema({
             vol.Optional(
                 CONF_POLL_INTERVAL,
-                default=current.get(CONF_POLL_INTERVAL, DEFAULT_POLL_INTERVAL)
+                default=data.get(CONF_POLL_INTERVAL, DEFAULT_POLL_INTERVAL)
             ): int,
             vol.Optional(
                 CONF_LOOKBACK_WINDOW,
-                default=current.get(CONF_LOOKBACK_WINDOW, DEFAULT_LOOKBACK_WINDOW)
+                default=data.get(CONF_LOOKBACK_WINDOW, DEFAULT_LOOKBACK_WINDOW)
+            ): int,
+            vol.Optional(
+                CONF_BIN_INTERVAL,
+                default=data.get(CONF_BIN_INTERVAL, DEFAULT_BIN_INTERVAL)
             ): int,
         })
 
-        return self.async_show_form(step_id="init", data_schema=schema)
+        return self.async_show_form(step_id="init", data_schema=schema, errors={})
+
+
 
